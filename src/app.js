047a9d7b55667070bca515f7ef405fa6be5196ef
atulsingla82 <asingla@wgu.edu>
2017-10-14 23:04:55 -0400

    Testing delete/update  actions

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ import {createStore} from 'redux';
 
 
 // Step 3 Define reducers
-const reducer = function (state=0, action){
+const reducer = function (state={books:[]}, action){
 switch(action.type){
-case "INCREMENT":
-return state + action.payload
+case "POST_BOOK":
+let books = state.books.concat(action.payload)
+return {books};
 break;
-case "DECREMENT":
-return state - action.payload
-break;
-}
+case "DELETE_BOOK":
+const currentBookToDelete = [...state.books]
 
+const indexToDelete = currentBookToDelete.findIndex(
+      function(book){
+
+            return book.id === action.payload.id;
+      }
+)
+
+return{books:[...currentBookToDelete.slice(0,indexToDelete),
+      ...currentBookToDelete.slice(indexToDelete+1)]}
+
+  break;
+}
 return state
 
 }
@@ -21,11 +32,40 @@ return state
 const store = createStore(reducer);
 
 store.subscribe (() => {
-console.log('current state:' + store.getState());
+console.log('current state:' , store.getState());
+//console.log('current price:' , store.getState()[1].price);
 })
 
 // Step 2 Create & dispatch actions 
 
-store.dispatch({type:"INCREMENT" , payload:1})
-store.dispatch({type:"INCREMENT" , payload:1})
-store.dispatch({type:"DECREMENT" , payload:1})
+store.dispatch({
+	type:"POST_BOOK" ,
+	 payload:[{
+       id:1,
+       title:'this is a title',
+       description:'this is the description',
+       price :33.33
+
+	 },
+	 {
+	   id:2,
+       title:'this is another title',
+       description:'this is the second description',
+       price :43.33
+
+	 }]
+
+	})
+
+  store.dispatch({
+	type:"DELETE_BOOK" ,
+	 payload:{
+       id:1,
+       title:'this is the third title',
+       description:'this is the description',
+       price :55.33
+
+	 }
+
+	})
+
---
+++ src/test.js
@@ -0,0 +1,8 @@
+var arr = [1,5,8,19]
+
+for ( var i =0 ; i < arr.length ; i++) {
+
+arr.reverse
+}
+
+console.log (arr)
